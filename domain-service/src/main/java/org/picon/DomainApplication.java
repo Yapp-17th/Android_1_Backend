6efe16804bbbd7fd8337dc8b6e4f9eb28dc0574c
rayoungji <dudwl7676@naver.com>
2020-09-16 16:05:40 +0900

    Eureka Server 생성 서비스 클라이언트 등록

---
+++ domain-service/src/main/java/org/picon/DomainApplication.java
@@ -0,0 +1,13 @@
+package org.picon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DomainApplication {
+    public static void main(String args[]){
+        SpringApplication.run(DomainApplication.class,args);
+    }
+}
