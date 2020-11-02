965597e48c64f8184ea20c1ead7483264baba592
rayoungji <dudwl7676@naver.com>
2020-11-02 14:02:04 +0900

    멀티파트파일 업로드 구현

---
+++ domain-service/src/main/java/org/picon/config/AmazonS3Config.java
@@ -0,0 +1,31 @@
+package org.picon.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonS3Config {
+
+    @Value("${spring.cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public AmazonS3 setS3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.AP_NORTHEAST_2)
+                .build();
+        return s3client;
+    }
+
+}
