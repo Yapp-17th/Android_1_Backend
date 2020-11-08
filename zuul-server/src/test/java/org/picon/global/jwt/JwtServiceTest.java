8aa1d86aa33b72b814b9b72241e45bdaa6f1ad07
ohjuntaek <201211734@pukyong.ac.kr>
2020-11-08 20:10:17 +0900

    테스트 제거 - 2

--- zuul-server/src/test/java/org/picon/global/jwt/JwtServiceTest.java
+++ zuul-server/src/test/java/org/picon/global/jwt/JwtServiceTest.java
@@ -1,23 +1,23 @@
-package org.picon.global.jwt;
-
-import com.netflix.discovery.converters.Auto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class JwtServiceTest {
-    @Autowired
-    JwtService jwtService;
-
-    @Test
-    @DisplayName("findEmailByToken 테스트 ")
-    void findEmailByTokenTest() {
-        String emailByToken = jwtService.findEmailByToken("eyJ0eXBlIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJFTUFJTCI6InRlc3RFbWFpbDEiLCJleHAiOjE2MDQ4MzI2NzYsImlhdCI6MTYwNDgzMjA3Nn0.3fwOpLaE6Wf9xL7Nhy-1Wu0yNZj8fMzna-d93o1J3Dg");
-        System.out.println("emailByToken = " + emailByToken);
-    }
-
-}
+//package org.picon.global.jwt;
+//
+//import com.netflix.discovery.converters.Auto;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class JwtServiceTest {
+//    @Autowired
+//    JwtService jwtService;
+//
+//    @Test
+//    @DisplayName("findEmailByToken 테스트 ")
+//    void findEmailByTokenTest() {
+//        String emailByToken = jwtService.findEmailByToken("eyJ0eXBlIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJFTUFJTCI6InRlc3RFbWFpbDEiLCJleHAiOjE2MDQ4MzI2NzYsImlhdCI6MTYwNDgzMjA3Nn0.3fwOpLaE6Wf9xL7Nhy-1Wu0yNZj8fMzna-d93o1J3Dg");
+//        System.out.println("emailByToken = " + emailByToken);
+//    }
+//
+//}
